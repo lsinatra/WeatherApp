2acd75b89be228fe9105e60be103001d67f74d98
lsinatra <83607794+lsinatra@users.noreply.github.com>
2022-01-11 21:30:22 +0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,18 +2,20 @@ import tkinter as tk
 import requests
 import time
 
-def getWeather():
-    city = textfield.get(canvas)
-    api = "api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=da6d05ee998089268813d864a1013c9a"
+def getWeather(canvas):
+    city = textField.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=da6d05ee998089268813d864a1013c9a"
+
     json_data = requests.get(api).json()
     condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
     min_temp = int(json_data['main']['temp_min'] - 273.15)
     max_temp = int(json_data['main']['temp_max'] - 273.15)
     pressure = json_data['main']['pressure']
     humidity = json_data['main']['humidity']
     wind = json_data['wind']['speed']
-    sunrise = time.strftime("%I:%M:%S", time.gmtime(json_data['sys'][sunrise] - 25200))
-    sunset = time.strftime("%I:%M:%S", time.gmtime(json_data['sys'][sunset] - 25200))
+    sunrise = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunrise'] - 25200))
+    sunset = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunset'] - 25200))
 
     final_info = condition + "\n" + str(temp) + "°C"
     final_data = "\n" + "Max Temp: " + str(max_temp) + "\n" + "Min Temp: " + str(min_temp) + "\n" + "Pressure: " + str(pressure) + "\n" + "Humidity: " + str(humidity) + "\n" + "Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
@@ -29,10 +31,10 @@ canvas.title("Weather App")
 f = ("poppins", 15, "bold")
 t = ("poppins", 35, "bold")
 
-textfield = tk.Entry(canvas, font = t)
-textfield.pack(pady = 20)
-textfield.focus()
-textfield.bind('<Return', getWeather)
+textField = tk.Entry(canvas, justify='center', width = 20, font = t)
+textField.pack(pady = 20)
+textField.focus()
+textField.bind('<Return>', getWeather)
 
 label1 = tk.Label(canvas, font = t)
 label1.pack()
